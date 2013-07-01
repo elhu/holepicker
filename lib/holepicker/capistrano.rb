42013a658ea9411f002beecccca8c0d4a5411597
Vincent Boisard <vincent.boisard@dimelo.com>
2013-07-01 19:04:16 +0200

    feature: added option to only warn about vulnerabilities before a deploy.

--- lib/holepicker/capistrano.rb
+++ lib/holepicker/capistrano.rb
@@ -3,6 +3,7 @@ require 'holepicker/scanner'
 Capistrano::Configuration.instance.load do
   set(:holepicker_offline, false) unless exists?(:holepicker_offline)
   set(:holepicker_ignored_gems, []) unless exists?(:holepicker_ignored_gems)
+  set(:holepicker_warn_only, false) unless exists?(:holepicker_warn_only)
 
   before "deploy:update_code", "holepicker"
 
@@ -17,7 +18,11 @@ Capistrano::Configuration.instance.load do
       gemfile_lock = "#{ENV['BUNDLE_GEMFILE']}.lock"
       success = HolePicker::Scanner.new(gemfile_lock, options).scan
       unless success
-        raise Capistrano::CommandError.new("HolePicker found vulnerabilities!")
+        if holepicker_warn_only
+          logger.important "HolePicker found vulnerabilities!"
+        else
+          raise Capistrano::CommandError.new("HolePicker found vulnerabilities!")
+        end
       end
     end
   end
